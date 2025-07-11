bcee95f676db0c4d2328ca2acda1705bc02f3451
Ali Piry <alipirydev@gmail.com>
2025-07-11 17:48:38 +0330

    update theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,7 @@
 return {
-  "rebelot/kanagawa.nvim",
-  lazy = false,
+  "olimorris/onedarkpro.nvim",
+  priority = 1000,
   config = function()
-    vim.cmd "colorscheme kanagawa-dragon"
+    vim.cmd "colorscheme onedark_dark"
   end,
 }
